77a5797929d301a736c7cd03255204ddff51401c
SAM <saha@free.fr>
2021-06-01 15:15:59 +0200

    TESinit

---
+++ src/main/java/com/exemple/demo/TestpFinal1Application.java
@@ -0,0 +1,13 @@
+package com.exemple.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpFinal1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestpFinal1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/exemple/demo/classes/Produit.java
@@ -0,0 +1,90 @@
+package com.exemple.demo.classes;
+
+public class Produit {
+	private int id, quantite;
+	private String titre;
+	private float prixu , prixv;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the quantite
+	 */
+	public int getQuantite() {
+		return quantite;
+	}
+	/**
+	 * @param quantite the quantite to set
+	 */
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+	/**
+	 * @return the titre
+	 */
+	public String getTitre() {
+		return titre;
+	}
+	/**
+	 * @param titre the titre to set
+	 */
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	/**
+	 * @return the prixu
+	 */
+	public float getPrixu() {
+		return prixu;
+	}
+	/**
+	 * @param prixu the prixu to set
+	 */
+	public void setPrixu(float prixu) {
+		this.prixu = prixu;
+	}
+	/**
+	 * @return the prixv
+	 */
+	public float getPrixv() {
+		return prixv;
+	}
+	/**
+	 * @param prixv the prixv to set
+	 */
+	public void setPrixv(float prixv) {
+		this.prixv = prixv;
+	}
+	public Produit(int id, int quantite, String titre, float prixu, float prixv) {
+		super();
+		this.id = id;
+		this.quantite = quantite;
+		this.titre = titre;
+		this.prixu = prixu;
+		this.prixv = prixv;
+	}
+	
+	
+	public Produit() {
+		super();
+	}
+	
+	@Override
+	public String toString() {
+		return "Produit [id=" + id + ", quantite=" + quantite + ", titre=" + titre + ", prixu=" + prixu + ", prixv="
+				+ prixv + "]";
+	}
+ 
+	
+	
+
+}
